eb2b5ff4d181b4313c54d3d357730dcc960592c7
Nghia Vo <vobanghia12@gmail.com>
2024-05-09 13:58:17 -0400

    login page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,21 @@
 'use server';
 
-export async function createInvoice(formData: FormData) {
-    
+import createSupabaseServerClient from './server';
+
+export async function signInWithEmalAndPassword(data: {
+  email: string;
+  password: string;
+}) {
+  const supabase = createSupabaseServerClient();
+
+  const result = (await supabase).auth.signInWithPassword({
+    email: data.email,
+    password: data.password,
+  });
+  return JSON.stringify(result);
+}
+
+export default async function readUserSession() {
+  const supabase = await createSupabaseServerClient();
+  return supabase.auth.getSession();
 }
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,4 +1,7 @@
+'use client';
 import { lusitana } from '@/app/ui/fonts';
+import { useState } from 'react';
+import { signInWithEmalAndPassword } from '../lib/actions';
 import {
   AtSymbolIcon,
   KeyIcon,
@@ -6,8 +9,27 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
+import { useRouter } from 'next/navigation';
 
 export default function LoginForm() {
+  const router = useRouter();
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const signIn = async (e: any) => {
+    //avoid null
+    e.preventDefault();
+    const data = {
+      email: username,
+      password: password,
+    };
+    const result = await signInWithEmalAndPassword(data);
+    const { error } = JSON.parse(result);
+    if (error?.message) console.log('cannot log in');
+    else {
+      console.log('login successfully');
+      router.push('/dashboard/school');
+    }
+  };
   return (
     <form className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
@@ -29,6 +51,7 @@ export default function LoginForm() {
                 type="email"
                 name="email"
                 placeholder="Enter your email address"
+                onChange={(e) => setUserName(e.target.value)}
                 required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -48,6 +71,7 @@ export default function LoginForm() {
                 type="password"
                 name="password"
                 placeholder="Enter password"
+                onChange={(e) => setPassword(e.target.value)}
                 required
                 minLength={6}
               />
@@ -55,19 +79,13 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <LoginButton />
+        <Button className="mt-4 w-full bg-green-700" onClick={signIn}>
+          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+        </Button>
         <div className="flex h-8 items-end space-x-1">
           {/* Add form errors here */}
         </div>
       </div>
     </form>
-  );
-}
-
-function LoginButton() {
-  return (
-    <Button className="mt-4 w-full">
-      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-    </Button>
   );
 }
