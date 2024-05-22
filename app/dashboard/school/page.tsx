7d134f9c0842509adf21ee4c6de5ceeb77193453
Nghia Vo <vobanghia12@gmail.com>
2024-05-22 12:35:37 -0400

    added dropdown menu interaction in the school ui

--- app/dashboard/school/page.tsx
+++ app/dashboard/school/page.tsx
@@ -8,18 +8,25 @@ import { CardConfidenceVisualizer } from '@/app/ui/dashboard/cards/general/card-
 import { useState } from 'react';
 import { CardThreeValue } from '@/app/ui/dashboard/cards/general/card-three-value';
 import { Tooltip } from '@nextui-org/react';
-import { DonutChartGender } from '@/app/ui/charts/total-demographics-charts';
-
-
+import {
+  AcademicChart,
+  BarChartEnglishLearner,
+  BarChartEthnicity,
+  BarChartTotal,
+  DonutChartGender,
+  EmotionalChart,
+  SocialChart,
+} from '@/app/ui/charts/total-demographics-charts';
+import Dropdown from '@/app/ui/Dropdown';
+import useRiskOptions from '@/hooks/useRiskOptions';
 function MidasRiskTooltipContent() {
   return (
-    <div>
-      Percentages of students at the three different MIDAS risk levels.
-    </div>
-  )
+    <div>Percentages of students at the three different MIDAS risk levels.</div>
+  );
 }
 
 export default async function Page() {
+  const riskOptions = useRiskOptions();
   // const mySaebrsEmoHigh = (await fetchMySaebrsData()).emo;
   // const mySaebrsSocHigh = (await fetchMySaebrsData()).soc;
   // const mySaebrsAcaHigh = (await fetchMySaebrsData()).aca;
@@ -27,107 +34,101 @@ export default async function Page() {
   // const numberOfStudents = await fetchStudents("Greco Middle School");
 
   const [midasRisk, setMidasRisk] = useState({
-    'low': '45%',
-    'some': '40%',
-    'high': '15%'
-  })
+    low: '45%',
+    some: '40%',
+    high: '15%',
+  });
 
   const [disciplineRisk, setDisciplineRisk] = useState({
-    'odrZero': '77%',
-    'odrSome': '23%',
-    'suspZero': '80%',
-    'suspSome': '20%'   
-  })
+    odrZero: '77%',
+    odrSome: '23%',
+    suspZero: '80%',
+    suspSome: '20%',
+  });
 
   // ASK SONJA WHAT THE VALUES FOR TEST RISK ARE
-  const [testRisk, setTestRisk] = useState({
-    
-  })
+  const [testRisk, setTestRisk] = useState({});
 
   const [saebrsRisk, setSaebrsRisk] = useState({
-    'saebrsTotal': ['60%', '25%', '15%'],
-    'mySaebrsTotal': ['54%', '33%', '13%'],
-    'saebrsEmotional': ['59%', '33%', '8%'], 
-    'mySaebrsEmotional': ['50%', '37%', '13%'],
-    'saebrsSocial': ['40%', '41%', '19%'], 
-    'mySaebrsSocial': ['40%', '39%', '17%'], 
-    'saebrsAcademic': ['72%', '16%', '12%'], 
-    'mySaebrsAcademic':['70%', '18%', '12%'],
-  })
+    saebrsTotal: ['60%', '25%', '15%'],
+    mySaebrsTotal: ['54%', '33%', '13%'],
+    saebrsEmotional: ['59%', '33%', '8%'],
+    mySaebrsEmotional: ['50%', '37%', '13%'],
+    saebrsSocial: ['40%', '41%', '19%'],
+    mySaebrsSocial: ['40%', '39%', '17%'],
+    saebrsAcademic: ['72%', '16%', '12%'],
+    mySaebrsAcademic: ['70%', '18%', '12%'],
+  });
 
   return (
     <main>
-
-      <div className='flex flex-row gap-4'>
+      <div className="flex gap-4">
         {/* LEFT COL */}
-        <div className="flex flex-col mb-4 basis-1/4">
-          <div className='flex flex-col'>
-            
-              <div className='pb-4'>
-                  <CardThreeValue 
-                    title="MIDAS Risk Scores" 
-                    values={[
-                      midasRisk['low'],
-                      midasRisk['some'],
-                      midasRisk['high']
-                    ]} 
-                  subtitles={['Low', 'Some', 'High']}
-                  tooltip={MidasRiskTooltipContent()}/>
-              </div>
-
-             <div className='pb-4'>
-              <CardConfidenceVisualizer confidence={90} confidenceThresholds={[85, 90, 95, 99]}/>
+        <div className="mb-4 flex basis-1/4 flex-col">
+          <div className="flex flex-col">
+            <div className="pb-4">
+              <CardThreeValue
+                title="MIDAS Risk Scores"
+                values={[
+                  midasRisk['low'],
+                  midasRisk['some'],
+                  midasRisk['high'],
+                ]}
+                subtitles={['Low', 'Some', 'High']}
+                tooltip={MidasRiskTooltipContent()}
+              />
             </div>
 
-            <div className='pb-4'>
+            <div className="pb-4">
+              <CardConfidenceVisualizer
+                confidence={90}
+                confidenceThresholds={[85, 90, 95, 99]}
+              />
+            </div>
+
+            <div className="pb-4">
               <CardDisciplinarySummary
-                title={'Disciplinary Action Summary'} 
+                title={'Disciplinary Action Summary'}
                 valuesTop={[
                   disciplineRisk['odrZero'],
-                  disciplineRisk['odrSome']
-                ]} 
-                subtitlesTop={['Zero', 'One Plus']} 
+                  disciplineRisk['odrSome'],
+                ]}
+                subtitlesTop={['Zero', 'One Plus']}
                 valuesBottom={[
                   disciplineRisk['suspZero'],
-                  disciplineRisk['suspSome']
+                  disciplineRisk['suspSome'],
                 ]}
-                subtitlesBottom={['Zero', 'One Plus']}            
+                subtitlesBottom={['Zero', 'One Plus']}
               />
             </div>
 
-            <div className='-mb-8'>
+            <div className="-mb-8">
               <CardTestScoreSummary
-                title={'Test Score Risk Summary'} 
-                valuesTop={['60%', '40%']} 
-                subtitlesTop={['Low', 'Some']} 
+                title={'Test Score Risk Summary'}
+                valuesTop={['60%', '40%']}
+                subtitlesTop={['Low', 'Some']}
                 valuesBottom={['55%', '45%']}
-                subtitlesBottom={['Low', 'Some']}            
+                subtitlesBottom={['Low', 'Some']}
               />
             </div>
-
           </div>
         </div>
+        <div className=" flex h-full w-full flex-col gap-y-28">
+          <div className="flex h-full w-full items-center justify-end">
+            <Dropdown />
+          </div>
 
-        <div className='flex flex-col basis-3/4 w-full'>
-            <div className='pb-4'>
-              <SaebrsSummary 
-                saebrsTotal={saebrsRisk['mySaebrsTotal']}
-                mySaebrsTotal={saebrsRisk['mySaebrsTotal']}
-                saebrsEmotional={saebrsRisk['mySaebrsTotal']}
-                mySaebrsEmotional={saebrsRisk['mySaebrsTotal']}
-                saebrsSocial={saebrsRisk['mySaebrsTotal']}
-                mySaebrsSocial={saebrsRisk['mySaebrsTotal']}
-                saebrsAcademic={saebrsRisk['mySaebrsTotal']}
-                mySaebrsAcademic={saebrsRisk['mySaebrsTotal']}
-              />
-            </div>
-
-            <div className='flex flex-col h-full'>
-              <PopToRiskCharts/>
-            </div>
-        </div>  
+          <div className="flex justify-center">
+            {riskOptions.isEthnicity && <BarChartEthnicity />}
+            {riskOptions.isEnglishLeaner && <BarChartEnglishLearner />}
+            {riskOptions.isGender && <DonutChartGender />}
+            {riskOptions.isAcademic && <AcademicChart />}
+            {riskOptions.isEmotional && <EmotionalChart />}
+            {riskOptions.isTotalScore && <BarChartTotal />}
+            {riskOptions.isSocial && <SocialChart />}
+          </div>
+        </div>
       </div>
     </main>
   );
 }
-
--- app/ui/charts/total-demographics-charts.tsx
+++ app/ui/charts/total-demographics-charts.tsx
@@ -1,110 +1,230 @@
-import { DonutChart, Card, Legend, BarChart } from '@tremor/react'
-import exp from 'constants'
- 
+import { DonutChart, Card, Legend, BarChart } from '@tremor/react';
+import exp from 'constants';
 
 const genderDataPlaceholder = [
   {
     Gender: 'Male',
-    'Total': 530,
+    Total: 530,
     'High Risk': 70,
     'Some Risk': 200,
-    'Low Risk': 180
+    'Low Risk': 180,
   },
   {
     Gender: 'Female',
-    'Total': 550,
+    Total: 550,
     'High Risk': 30,
     'Some Risk': 56,
-    'Low Risk': 300
-  }
-]
+    'Low Risk': 300,
+  },
+];
 
 const ethnicityDataPlaceholder = [
   {
     Ethnicity: 'White',
     'High Risk': 58,
     'Some Risk': 200,
-    'Low Risk': 100
+    'Low Risk': 100,
   },
   {
     Ethnicity: 'Hispanic',
     'High Risk': 20,
     'Some Risk': 150,
-    'Low Risk': 130
+    'Low Risk': 130,
   },
   {
     Ethnicity: 'Other POC',
     'High Risk': 33,
     'Some Risk': 156,
-    'Low Risk': 200
-  }
-]
+    'Low Risk': 200,
+  },
+];
 
 const englishLearnerDataPlaceholder = [
   {
     ELL: 'ELL',
     'High Risk': 58,
     'Some Risk': 200,
-    'Low Risk': 100
+    'Low Risk': 100,
   },
   {
     ELL: 'Not ELL',
     'High Risk': 20,
     'Some Risk': 150,
-    'Low Risk': 130
-  }
-]
+    'Low Risk': 130,
+  },
+];
 
-const colors = ['rose-500', 'yellow-400', 'green-500']
+const totalPlaceholder = [
+  {
+    Saebrs: 'Saebrs',
+    'High Risk': 58,
+    'Some Risk': 200,
+    'Low Risk': 100,
+  },
+  {
+    MySaebrs: 'MySaebrs',
+    'High Risk': 20,
+    'Some Risk': 150,
+    'Low Risk': 130,
+  },
+];
+
+const emotionalPlaceholder = [
+  {
+    Saebrs: 'Saebrs',
+    'High Risk': 58,
+    'Some Risk': 200,
+    'Low Risk': 100,
+  },
+  {
+    MySaebrs: 'MySaebrs',
+    'High Risk': 20,
+    'Some Risk': 150,
+    'Low Risk': 130,
+  },
+];
+
+const sociallPlaceholder = [
+  {
+    Saebrs: 'Saebrs',
+    'High Risk': 58,
+    'Some Risk': 200,
+    'Low Risk': 100,
+  },
+  {
+    MySaebrs: 'MySaebrs',
+    'High Risk': 20,
+    'Some Risk': 150,
+    'Low Risk': 130,
+  },
+];
+
+const academicPlaceholder = [
+  {
+    Saebrs: 'Saebrs',
+    'High Risk': 58,
+    'Some Risk': 200,
+    'Low Risk': 100,
+  },
+  {
+    MySaebrs: 'MySaebrs',
+    'High Risk': 20,
+    'Some Risk': 150,
+    'Low Risk': 130,
+  },
+];
+
+const colors = ['rose-500', 'yellow-400', 'green-500'];
 
 export function DonutChartGender() {
   return (
-
-      <BarChart
-        className='min-h-full h-max'
-        data={genderDataPlaceholder}
-        index='Gender'
-        categories={['High Risk', 'Some Risk', 'Low Risk']}
-        colors={colors}
-        layout='horizontal'
-        yAxisWidth={50}
-        stack={true}
-        
-      />
-
-  )
+    <BarChart
+      className="transition md:h-[30rem] md:w-[60rem]"
+      data={genderDataPlaceholder}
+      index="Gender"
+      categories={['High Risk', 'Some Risk', 'Low Risk']}
+      colors={colors}
+      layout="horizontal"
+      yAxisWidth={50}
+      stack={true}
+      barCategoryGap={140}
+    />
+  );
 }
 
 export function BarChartEthnicity() {
   return (
-
+    <div className="flex items-center justify-center">
       <BarChart
-        className='min-h-full h-max'
+        className="transition md:h-[30rem] md:w-[60rem]"
         data={ethnicityDataPlaceholder}
-        index='Ethnicity'
+        index="Ethnicity"
         categories={['High Risk', 'Some Risk', 'Low Risk']}
         colors={colors}
-        layout='horizontal'
-        yAxisWidth={50}
+        layout="horizontal"
         stack={true}
-        
+        barCategoryGap={70}
       />
-  )
+    </div>
+  );
 }
 
 export function BarChartEnglishLearner() {
   return (
-
     <BarChart
-      className='min-h-full h-max'
+      className="transition md:h-[30rem] md:w-[60rem]"
       data={englishLearnerDataPlaceholder}
-      index='ELL'
+      index="ELL"
       categories={['High Risk', 'Some Risk', 'Low Risk']}
       colors={colors}
-      layout='horizontal'
+      layout="horizontal"
       yAxisWidth={50}
       stack={true}
+      barCategoryGap={140}
     />
-  )
+  );
 }
 
+export function BarChartTotal() {
+  return (
+    <BarChart
+      className="transition md:h-[30rem] md:w-[60rem]"
+      data={totalPlaceholder}
+      index="Total"
+      categories={['High Risk', 'Some Risk', 'Low Risk']}
+      colors={colors}
+      layout="horizontal"
+      yAxisWidth={50}
+      stack={true}
+      barCategoryGap={140}
+    />
+  );
+}
+
+export function EmotionalChart() {
+  return (
+    <BarChart
+      className="transition md:h-[30rem] md:w-[60rem]"
+      data={emotionalPlaceholder}
+      index="Emotional"
+      categories={['High Risk', 'Some Risk', 'Low Risk']}
+      colors={colors}
+      layout="horizontal"
+      yAxisWidth={50}
+      stack={true}
+      barCategoryGap={140}
+    />
+  );
+}
+
+export function SocialChart() {
+  return (
+    <BarChart
+      className="transition md:h-[30rem] md:w-[60rem]"
+      data={emotionalPlaceholder}
+      index="Social"
+      categories={['High Risk', 'Some Risk', 'Low Risk']}
+      colors={colors}
+      layout="horizontal"
+      yAxisWidth={50}
+      stack={true}
+      barCategoryGap={140}
+    />
+  );
+}
+
+export function AcademicChart() {
+  return (
+    <BarChart
+      className="transition md:h-[30rem] md:w-[60rem]"
+      data={emotionalPlaceholder}
+      index="Academic"
+      categories={['High Risk', 'Some Risk', 'Low Risk']}
+      colors={colors}
+      layout="horizontal"
+      yAxisWidth={50}
+      stack={true}
+      barCategoryGap={140}
+    />
+  );
+}
